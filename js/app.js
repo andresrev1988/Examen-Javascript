4a4e6a3349324f3f6be64c58fe0180e134d35b77
andresrev1988 <andres.revelo@icloud.com>
2017-04-24 19:57:05 -0500

    5, 6, 7, 9
    completado

--- js/app.js
+++ js/app.js
@@ -1,16 +1,79 @@
 function cambiarTamañoAccionTecla(elemento){
-    elemento.style.height = "61px";
+    if(elemento.id=="mas"){
+      elemento.style.height = "95%";
+    }else{
+      elemento.style.height = "61px";
+    }
 }
 function retornarTamañoAccionTecla(elemento){
-  elemento.style.height = "62.91px";
+  if(elemento.id=="mas"){
+    elemento.style.height = "100%";
+  }else{
+    elemento.style.height = "62.91px";
+  }
 }
-function cambiarTamañoAccionMAS(elemento){
-    elemento.style.height = "95%";
+function PresionaTecla(boton){
+  var tecla=boton.id;
+  switch (tecla) {
+
+    case "on":
+        alert("on");
+        break;
+    case "sign":
+        alert("sign");
+        break;
+    case "raiz":
+        alert("raiz");
+        break;
+    case "dividido":
+        alert("dividido");
+        break;
+    case "por":
+        alert("por");
+        break;
+    case "menos":
+        alert("resta");
+        break;
+    case "mas":
+        alert("suma");
+        break;
+    case "igual":
+        alert("igual");
+        break;
+    default:
+        insertaNumero(boton);
+      }
 }
-function retornarTamañoAccionMAS(elemento){
-  elemento.style.height = "100%";
+
+function insertaNumero(boton){
+  tecla = boton.id;
+      var pantalla = document.getElementById("display").innerHTML;
+      var numero = "0";
+      //valida punto
+      var flag = true;
+      if (pantalla.indexOf('.') > -1){
+        flag = false;
+      }
+      //valida el 0
+      if(pantalla == "0"){
+        if(tecla=="punto"&&flag){
+          numero="0.";
+        }else{
+          numero = tecla;
+        }
+      }else{
+        if(tecla=="punto"&&flag){
+          numero = pantalla + ".";
+        }else{
+          numero = pantalla + tecla;
+        }
+      }
+      //escribe el numero en la pantalla
+      if(numero.length<=8){
+          document.getElementById("display").innerHTML=numero;
+      }
 }
-/////////////////////////////
+////////////////////////
 var Calculadora = {
   init: function(){
     this.asignarEventosBotones('tecla');
@@ -18,27 +81,18 @@ var Calculadora = {
   asignarEventosBotones: function(selector){
       var botonesPagina = document.getElementsByClassName(selector);
       for (var i = 0; i < botonesPagina.length; i++) {
-        if(botonesPagina[i].id=="mas"){
-          botonesPagina[i].onmousedown = this.eventoClickMAS;
-          botonesPagina[i].onmouseup = this.eventoSueltaMAS;
-        }else{
-        botonesPagina[i].onmousedown = this.eventoClickTecla;
-        botonesPagina[i].onmouseup = this.eventoSueltaTecla;
-        }
+        botonesPagina[i].onmousedown = this.eventoPresionaTecla;
+        botonesPagina[i].onmouseup= this.eventoSueltaTecla;
       }
   },
-  eventoClickTecla: function(event){
+    eventoPresionaTecla: function(event){
     cambiarTamañoAccionTecla(event.target);
   },
   eventoSueltaTecla: function(event){
     retornarTamañoAccionTecla(event.target);
+    //solo inserta el numero cuando se suelte el click del mouse
+    PresionaTecla(event.target);
   },
-  eventoClickMAS: function(event){
-    cambiarTamañoAccionMAS(event.target);
-  },
-  eventoSueltaMAS: function(event){
-    retornarTamañoAccionMAS(event.target);
-  }
 }
 
 Calculadora.init();
